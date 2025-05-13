11477274d8bb71e70594f4de010d87d31225d92f
Christophe Benedetti <christophe.benedetti15@gmail.com>
2025-05-13 22:15:13 +0200

    update crc polynome to comply easier with android crc32 lib

--- common/crc32/crc32.h
+++ common/crc32/crc32.h
@@ -8,30 +8,12 @@
 ////////////////////////////////////////////////////////////
 
 /*
- *  Castagnoli CRC32C Checksum Algorithm
+ *  Standard CRC32 Checksum Algorithm (zlib compatible)
  *
- *  Polynomial: 0x11EDC6F41
+ *  Polynomial: 0x04C11DB7
  *
- *  Castagnoli93: Guy Castagnoli and Stefan Braeuer and Martin Herrman
- *               "Optimization of Cyclic Redundancy-Check Codes with 24
- *                 and 32 Parity Bits",IEEE Transactions on Communication,
- *                Volume 41, Number 6, June 1993
- *
- *  Copyright (c) 2013 Red Hat, Inc.,
- *
- *  Authors:
- *   Jeff Cody <jcody@redhat.com>
- *
- *  Based on the Linux kernel cryptographic crc32c module,
- *
- *  Copyright (c) 2004 Cisco Systems, Inc.
- *  Copyright (c) 2008 Herbert Xu <herbert@gondor.apana.org.au>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
- *
+ *  Used in Ethernet, gzip, zip, PNG, etc.
+ *  Compatible with java.util.zip.CRC32 and zlib crc32
  */
 
 #ifndef CRC32_H_
@@ -40,24 +22,12 @@
 #include <stdint.h>
 #include <stddef.h>
 
-
 ////////////////////////////////////////////////////////////
-/// @brief CRC32C polynomial
-////////////////////////////////////////////////////////////
-const uint32_t k_ulCrc32cPolynomial = 0x11EDC6F41;
-
-////////////////////////////////////////////////////////////
-/// @brief CRC32C initial value
-////////////////////////////////////////////////////////////
-const uint32_t k_ulCrc32cInitialValue = 0xffffffff;
-
-////////////////////////////////////////////////////////////
-/// @brief Calculate the CRC32C checksum of a buffer
+/// @brief Calculate the CRC32 checksum of a buffer (zlib compatible)
 /// @param data The data buffer to calculate the checksum for
 /// @param length The length of the data buffer
-/// @return The calculated CRC32C checksum
+/// @return The calculated CRC32 checksum
 ////////////////////////////////////////////////////////////
-uint32_t crc32c(const uint8_t *data, unsigned int length);
-
+uint32_t compute_crc32(const uint8_t *data, unsigned long length);
 
 #endif // CRC32_H_
